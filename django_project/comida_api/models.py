99ba6fbc6c952284b2ca3e4902bab25e0319352c
popokobe <yuki.n3kobe@gmail.com>
2020-02-01 02:58:29 +0900

    modelsのPOSTにカテゴリーを追加

--- django_project/comida_api/models.py
+++ django_project/comida_api/models.py
@@ -6,11 +6,33 @@ class User(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.username
+
 class Friend(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
     friend = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friend')
 
 class Post(models.Model):
+    CATEGORY_CHOICES = [
+        ('JPN', '和食'),
+        ('LOCAL', '郷土料理'),
+        ('SUSHI', '寿司'),
+        ('RAMEN', 'ラーメン類'),
+        ('CURRY', 'カレー'),
+        ('FA', 'ファミレス・ファストフード'),
+        ('MEAT', '焼肉・ステーキ等の肉関連'),
+        ('BAR', '居酒屋・バー'),
+        ('CHN', '中華'),
+        ('WES', '洋食'),
+        ('ITA', 'イタリアン'),
+        ('FRA', 'フレンチ'),
+        ('ASIA', 'アジア・エスニック'),
+        ('CAFE', 'カフェ'),
+        ('SWEETS', 'スイーツ'),
+        ('OTHER', 'その他'),
+    ]
+
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -18,7 +40,10 @@ class Post(models.Model):
     area = models.CharField(max_length=50)
     address = models.CharField(max_length=50, blank=True)
     dish = models.CharField(max_length=100, blank=True)
-    category = models.CharField(max_length=20)
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES
+    )
     expense = models.IntegerField(blank=True)
     note = models.TextField(blank=True)
     img = models.ImageField(
