aea23f75e14ef4108762e969af06d37c8c418bb5
popokobe <yuki.n3kobe@gmail.com>
2020-02-02 21:26:02 +0900

    serializersにHyperlinkedを追加

--- django_project/comida_api/serializers.py
+++ django_project/comida_api/serializers.py
@@ -1,17 +1,18 @@
 from rest_framework import serializers
 from .models import User, Friend, Post
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = (
             'id',
+            'url',
             'username',
             'email',
             'password'
         )
 
-class FriendSerializer(serializers.ModelSerializer):
+class FriendSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Friend
         fields = (
@@ -19,11 +20,12 @@ class FriendSerializer(serializers.ModelSerializer):
             'friend'
         )
 
-class PostSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
         fields = (
             'id',
+            'url',
             'author',
             'created_at',
             'updated_at',
