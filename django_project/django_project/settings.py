7b0a8ffd6959e9610c18a874a4a3b82be9b62fea
popokobe <yuki.n3kobe@gmail.com>
2020-02-02 22:36:13 +0900

    フロントとバックを繋いだ

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'comida_api'
+    'comida_api',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +125,10 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# 許可するオリジン
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
+# レスポンスを公開する
+CORS_ALLOW_CREDENTIALS = True
